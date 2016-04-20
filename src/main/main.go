8102a48bbbd275f94efcb12862865ee433ece6c1
grecocd <cassiodgreco@gmail.com>
2016-04-20 23:04:12 +0100

    Separated flags' logic from main package and created test

--- src/main/main.go
+++ src/main/main.go
@@ -1,75 +1,27 @@
 package main
 
 import (
-	"errors"
+	"flags"
 	"fmt"
-	"os"
 )
 
-type FlagDescription struct {
-	Flag        string
-	Description string
-	Valid       bool
-	Execute     func(...interface{})
-}
-
-var AcceptedFlags map[string]FlagDescription = map[string]FlagDescription{
-	"-h": FlagDescription{Flag: "-h", Description: "Help command", Valid: true, Execute: Help},
-	"-t": FlagDescription{Flag: "-t", Description: "Test command", Valid: false, Execute: Test},
-}
-
 func main() {
-	flags, err := validateFlags(getFlags())
+	inputFlags, err := flags.ValidateFlags(flags.GetFlags())
 	if err == nil {
-		for _, flag := range flags {
+		for _, flag := range inputFlags {
 			if flag == "-h" {
-				AcceptedFlags[flag].Execute(getAcceptedFlags())
+				problem := flags.AcceptedFlags[flag].Execute(flags.GetAcceptedFlags())
+				if problem != nil {
+					fmt.Println("\nThere was an error executing your command: " + problem.Error())
+				}
 			} else {
-				AcceptedFlags[flag].Execute()
+				problem := flags.AcceptedFlags[flag].Execute()
+				if problem != nil {
+					fmt.Println("\nThere was an error executing your command: " + problem.Error())
+				}
 			}
 		}
 	} else {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
-}
-
-func getFlags() []string {
-	return os.Args[1:]
-}
-
-func validateFlags(flags []string) ([]string, error) {
-	for _, flag := range flags {
-		if string(flag[0]) != "-" {
-			return []string{}, errors.New("Flag '" + flag + "' is not formatted correctly. Flags must start with a '-' in front. Run command with -h for help.")
-		}
-		if _, exists := AcceptedFlags[flag]; exists == false || !AcceptedFlags[flag].Valid {
-			return []string{}, errors.New("Flag '" + flag + "' does not exist. Run command with -h for help.")
-		}
-	}
-	return flags, nil
-}
-
-func getAcceptedFlags() []FlagDescription {
-	var accepted []FlagDescription
-	for _, flag := range AcceptedFlags {
-		if flag.Valid {
-			accepted = append(accepted, FlagDescription{Flag: flag.Flag, Description: flag.Description})
-		}
-	}
-	return accepted
-}
-
-func Help(flags ...interface{}) {
-	fmt.Println("Supported flags: ")
-	fmt.Println("\n")
-	for _, flag := range flags {
-		commands := flag.([]FlagDescription)
-		for _, command := range commands {
-			fmt.Println(command.Flag + " " + command.Description + "\n")
-		}
-	}
-}
-
-func Test(input ...interface{}) {
-
 }
